d650593be0fcca42df72c7f79764d01195db4857
TheIrishPizzaGuy <kadenzarowny@gmail.com>
2020-01-18 18:12:22 -0800

    Added Main Unity Files
    Scene basic gyro

---
+++ Unity Files/Main Unity/Assets/Scripts/Gravity.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour
+{
+
+    [SerializeField]
+    private float gravityX = 0f;
+    [SerializeField]
+    private float gravityY = -9.8f;
+
+    void Start()
+    {
+        Screen.orientation = ScreenOrientation.LandscapeLeft;//or right for right landscape }
+    }
+
+        void FixedUpdate()
+    {
+        Physics2D.gravity = new Vector2(gravityX, gravityY);
+    }
+}
