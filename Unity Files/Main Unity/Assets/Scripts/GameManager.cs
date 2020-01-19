e2ac3b3c472282b5c560e79a40c595347ac138d3
ar2rworld <60049241+ar2rworld@users.noreply.github.com>
2020-01-18 21:00:09 -0800

    GM

---
+++ Unity Files/Main Unity/Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public bool isPaused;
+    void Start()
+    {
+        Time.timeScale = 0.0f;
+    }
+
+        void FixedUpdate()
+    {
+        isPausedM();
+    }
+    void isPausedM()
+    {
+        if (!isPaused)
+        {
+            Time.timeScale = 1.0f;
+        }
+        else
+        {
+            Time.timeScale = 0.0f;
+        }
+    }
+}
